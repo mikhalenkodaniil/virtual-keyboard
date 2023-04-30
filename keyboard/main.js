71de2b95ea8a7a096613840a904ecac746f7c9e6
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-04-30 23:39:59 +0300

    feat(minimal-scope):generation of DOM & highlights

---
+++ keyboard/main.js
@@ -0,0 +1,53 @@
+const keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 92, 122, 120, 99, 118, 98, 110, 109, 46, 44, 47, 32];
+// document.onkeypress = function(event){
+//     // console.log(event);
+//     keyboard.push(event.charCode);
+//     console.log(keyboard);
+// }
+//Создание div в html:
+var as='<div class="all-keyboard"><div id="keyboard"></div></div>';
+var div = document.createElement("DIV");
+div.innerHTML = as;
+document.getElementsByTagName('body')[0].appendChild(div);
+
+
+function init(){
+    let out = '';
+    for (let i = 0; i < keyboard.length; i++){
+        if (i==13){
+            out +='<div class="clearfix"></div>';
+        }
+        if (i==26){
+            out +='<div class="clearfix"></div>';
+        }
+        if (i==37){
+            out +='<div class="clearfix"></div>';
+        }
+        if (i==48){
+            out +='<div class="clearfix"></div>';
+        }
+        out +='<div class="k-key" data="'+keyboard[i]+'">'+String.fromCharCode(keyboard[i])+'</div>';
+    }
+    document.querySelector('#keyboard').innerHTML = out;
+}
+init();
+
+document.onkeypress = function(event){
+    console.log(event.code);
+    console.log(event.keyCode);
+    document.querySelectorAll('#keyboard .k-key').forEach(function(element){
+        element.classList.remove('active');
+    });
+    document.querySelector('#keyboard .k-key[data="'+event.keyCode+'"]').classList.add('active');
+}
+
+document.querySelectorAll('#keyboard .k-key').forEach(function(element){
+    element.onclick = function(event){
+        document.querySelectorAll('#keyboard .k-key').forEach(function(element){
+            element.classList.remove('active');
+        });
+        let code = this.getAttribute('data');
+        this.classList.add('active');
+        console.log(code);
+    }
+});
