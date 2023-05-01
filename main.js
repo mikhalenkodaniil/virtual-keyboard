a747d3f5c3b7884b7a141639bcb0d1f2a5a4d383
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-05-01 19:36:32 +0300

    feat(basic): add rus generation

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 46, 44, 47, 32];
-// let zeyboard = [];
+const keyboardeng = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 46, 44, 47, 32];
+const keyboardrus = [1105, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 1081, 1094, 1091, 1082, 1077, 1085, 1075, 1096, 1097, 1079, 1093, 1098, 92, 1092, 1099, 1074, 1072, 1087, 1088, 1086, 1083, 1076, 1078, 1101, 1103, 1095, 1089, 1084, 1080, 1090, 1100, 1073, 1102, 46, 32];
+
+// let keyboardrus = [];
 // document.onkeypress = function(event){
 //     // console.log(event);
-//     zeyboard.push(event.code);
-//     console.log(zeyboard);
+//     keyboardrus.push(event.charCode);
+//     console.log(keyboardrus);
 // }
 
 
@@ -19,9 +21,9 @@ div.innerHTML = as;
 document.getElementsByTagName('body')[0].appendChild(div);
 
 
-function init(){
+function initeng(){
     let out = '';
-    for (let i = 0; i < keyboard.length; i++){
+    for (let i = 0; i < keyboardeng.length; i++){
         if (i==13){
             out +='<div class="k-key" data="spec" id="backspace">BackSpace</div>';
         }
@@ -38,7 +40,7 @@ function init(){
             out +='<div class="k-key" data="spec" id="enter">Enter</div>';
             out +='<div class="clearfix"></div>';
             out +='<div class="k-key" data="spec" id="l-shift">Shift</div>';
-            out +='<div class="k-key" data="92" id="l-slash">'+String.fromCharCode(keyboard[25])+'</div>';
+            out +='<div class="k-key" data="92" id="l-slash">'+String.fromCharCode(keyboardeng[25])+'</div>';
         }
         if (i==47){
             out +='<div class="k-key" data="spec" id="up"><img src="icons/arrow.svg" alt="" width="50px"></div>';
@@ -48,8 +50,8 @@ function init(){
             out +='<div class="k-key" data="spec" id="win">Win</div>';
             out +='<div class="k-key" data="spec" id="l-alt">Alt</div>';
         }
-        out +='<div class="k-key" data="'+keyboard[i]+'">'+String.fromCharCode(keyboard[i])+'</div>';
-        if (i==(keyboard.length-1)){
+        out +='<div class="k-key" data="'+keyboardeng[i]+'">'+String.fromCharCode(keyboardeng[i])+'</div>';
+        if (i==(keyboardeng.length-1)){
             out +='<div class="k-key" data="spec" id="r-alt">Alt</div>';
             out +='<div class="k-key" data="spec" id="r-ctrl">Ctrl</div>';
             out +='<div class="k-key" data="spec" id="left"><img src="icons/arrow.svg" alt="" width="50px"></div>';
@@ -59,7 +61,50 @@ function init(){
     }
     document.querySelector('#keyboard').innerHTML = out;
 }
-init();
+initeng();
+
+
+function initrus(){
+    let out = '';
+    for (let i = 0; i < keyboardrus.length; i++){
+        if (i==13){
+            out +='<div class="k-key" data="spec" id="backspace">BackSpace</div>';
+        }
+        if (i==13){
+            out +='<div class="clearfix"></div>';
+            out +='<div class="k-key" data="spec" id="tab">Tab</div>';
+        }
+        if (i==26){
+            out +='<div class="k-key" data="spec" id="del">Del</div>';
+            out +='<div class="clearfix"></div>';
+            out +='<div class="k-key" data="spec" id="caps-lock">Caps Lock</div>';
+        }
+        if (i==37){
+            out +='<div class="k-key" data="spec" id="enter">Enter</div>';
+            out +='<div class="clearfix"></div>';
+            out +='<div class="k-key" data="spec" id="l-shift">Shift</div>';
+            out +='<div class="k-key" data="92" id="l-slash">'+String.fromCharCode(keyboardrus[25])+'</div>';
+        }
+        if (i==47){
+            out +='<div class="k-key" data="spec" id="up"><img src="icons/arrow.svg" alt="" width="50px"></div>';
+            out +='<div class="k-key" data="spec" id="r-shift">Shift</div>';
+            out +='<div class="clearfix"></div>';
+            out +='<div class="k-key" data="spec" id="l-ctrl">Ctrl</div>';
+            out +='<div class="k-key" data="spec" id="win">Win</div>';
+            out +='<div class="k-key" data="spec" id="l-alt">Alt</div>';
+        }
+        out +='<div class="k-key" data="'+keyboardrus[i]+'">'+String.fromCharCode(keyboardrus[i])+'</div>';
+        if (i==(keyboardrus.length-1)){
+            out +='<div class="k-key" data="spec" id="r-alt">Alt</div>';
+            out +='<div class="k-key" data="spec" id="r-ctrl">Ctrl</div>';
+            out +='<div class="k-key" data="spec" id="left"><img src="icons/arrow.svg" alt="" width="50px"></div>';
+            out +='<div class="k-key" data="spec" id="down"><img src="icons/arrow.svg" alt="" width="50px"></div>';
+            out +='<div class="k-key" data="spec" id="right"><img src="icons/arrow.svg" alt="" width="50px"></div>';
+        }
+    }
+    document.querySelector('#keyboard').innerHTML = out;
+}
+// initrus();
 
 
 
